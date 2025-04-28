8cf49bd723ef4f95e5a7f118789b814506d2d8b3
Joel Drapper <j.drapper@plane.com>
2025-04-28 17:07:55 +0100

    Allow ivar definitions anywhere

--- test/example.rb
+++ test/example.rb
@@ -1,41 +1,15 @@
 # frozen_string_literal: true
 
 class Example
-	class << self
-		def foo
-			@foo = 1
-		end
-	end
-
 	def initialize
 		@bar = "bar"
 	end
 
-	def self.bar
-		@bar = 1
-	end
-
 	def foo
-		@foo = 1
-	end
-
-	def foo=(value)
-		@foo = value
+		@foo
 	end
 
 	def bar
 		@bar
-	end
-
-	def bar=(value)
-		@bar = value
-	end
-
-	def lazy
-		if defined?(@lazy)
-			@lazy
-		else
-			@lazy = 1
-		end
 	end
 end
