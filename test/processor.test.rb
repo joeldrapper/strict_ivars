9d9ff1b984ef93aef128b2e282ef4fc1d0c8a90f
Joel Drapper <j.drapper@plane.com>
2025-04-30 02:16:31 +0100

    More processor tests

---
+++ test/processor.test.rb
@@ -0,0 +1,151 @@
+# frozen_string_literal: true
+
+test "basic" do
+	processed = StrictIvars::Processor.call(<<~RUBY)
+		def foo
+			@foo
+		end
+	RUBY
+
+	assert_equal_ruby processed, <<~RUBY
+		def foo
+			#{guarded(:@foo)}
+		end
+	RUBY
+end
+
+test "conditional" do
+	processed = StrictIvars::Processor.call(<<~RUBY)
+		def foo
+			bar if @foo
+		end
+	RUBY
+
+	assert_equal_ruby processed, <<~RUBY
+		def foo
+			bar if #{guarded(:@foo)}
+		end
+	RUBY
+end
+
+test "multiple reads" do
+	processed = StrictIvars::Processor.call(<<~RUBY)
+		def foo
+			@foo
+			@foo
+		end
+	RUBY
+
+	assert_equal_ruby processed, <<~RUBY
+		def foo
+			#{guarded(:@foo)}
+			@foo
+		end
+	RUBY
+end
+
+test "if conditional" do
+	processed = StrictIvars::Processor.call(<<~RUBY)
+		def foo
+			@a
+
+			if @b
+				@a
+				@b
+				@c
+			else
+				@a
+				@b
+				@c
+			end
+
+			@a
+			@b
+			@c
+		end
+	RUBY
+
+	assert_equal_ruby processed, <<~RUBY
+		def foo
+			#{guarded(:@a)}
+
+			if #{guarded(:@b)}
+				@a
+				@b
+				#{guarded(:@c)}
+			else
+				@a
+				@b
+				#{guarded(:@c)}
+			end
+
+			@a
+			@b
+			#{guarded(:@c)}
+		end
+	RUBY
+end
+
+test "case" do
+	processed = StrictIvars::Processor.call(<<~RUBY)
+		@a
+
+		case @b
+		when @c
+			@a
+			@b
+			@c
+			@d
+		else
+			@a
+			@b
+			@c
+			@d
+		end
+
+		@a
+		@b
+		@c
+		@d
+	RUBY
+
+	assert_equal_ruby processed, <<~RUBY
+		#{guarded(:@a)}
+
+		case #{guarded(:@b)}
+		when #{guarded(:@c)}
+			@a
+			@b
+			@c
+			#{guarded(:@d)}
+		else
+			@a
+			@b
+			#{guarded(:@c)}
+			#{guarded(:@d)}
+		end
+
+		@a
+		@b
+		#{guarded(:@c)}
+		#{guarded(:@d)}
+	RUBY
+end
+
+test "open method definition def" do
+	processed = StrictIvars::Processor.call(<<~RUBY)
+		def foo
+			@a
+			@a
+	RUBY
+
+	assert_equal_ruby processed, <<~RUBY
+		def foo
+			#{guarded(:@a)}
+			@a
+	RUBY
+end
+
+def guarded(name)
+	"(defined?(#{name.name}) ? #{name.name} : (::Kernel.raise(::StrictIvars::NameError.new('Undefined instance variable #{name.name}'))))"
+end
